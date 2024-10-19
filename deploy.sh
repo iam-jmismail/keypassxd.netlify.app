fb7d9fe07dc5507f8e12e86b87dc82ff9575d8b1
iam-jmismail <jmismail628@gmail.com>
2024-10-19 18:12:11 +0530

    feat: uploaded deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+# Replace placeholder with GTM ID
 sed -i "s/%%GTM_ID%%/$GTM_ID/g" index.html
+# Copy modified HTML to the public directory
+cp index.html public/
+cp index.css public/
+cp index.js public/
+cp robots.txt public/
+cp words.json public/
+
